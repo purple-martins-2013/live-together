abdcd73f4637600474d5f2e431284cc68ae0192e
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-05 16:58:49 -0700

    add associations to house, user and chores models

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,4 +2,7 @@ class House < ActiveRecord::Base
 
   validates_presence_of :name, :address
 
+  has_many :users
+  has_many :chores
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :house
+
 end
