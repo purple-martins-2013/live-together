8963494ecb93db1b4ceaf0f3f5eb80906d986740
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-05 15:04:46 -0700

    add more to user can sign up feature spec

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
 
-  feature "user can sign up" do
+feature "user can sign up" do
 
-  
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    visit new_user_registration_path
+  end
+
+  it "successfully displays the form page" do
+    expect(page).to have_content "Sign up"
+  end
+
+  it "successfully creates a new user with valid info" do
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    fill_in 'user_password_confirmation', with: user.password
+    click_button 'Sign up'
+    expect(page).to have_content 'Logout'
+    expect(page).to have_content 'Welcome!'
+  end
+
+  it "displays an error message when a user is created without valid info" do
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Sign up'
+    expect(page).to have_content 'Error'
+  end
 
 end
