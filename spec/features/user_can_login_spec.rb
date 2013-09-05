b21e2ec294edcd7d7551e227341ad43e30c474d0
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-05 15:23:33 -0700

    finish feature tests for user logging in

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+feature "user can login" do
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    visit new_user_session_path
+  end
+
+  it "successfully displays the form page" do
+    expect(page).to have_content "Sign in"
+  end
+
+  it "successfully logs in a user with valid info" do
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Sign in'
+    expect(page).to have_content 'Logout'
+    expect(page).to have_content 'Signed in successfully'
+  end
+
+  it "displays an error message when a user is logged in without valid info" do
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: 'pass'
+    click_button 'Sign in'
+    expect(page).to have_content 'Invalid'
+  end
+
+end
