a5752120cdf847e47bb4dfa8b5fae25c25534a49
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-05 15:33:11 -0700

    add unit tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe "validations" do
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should allow_value('example@example.com').for(:email) }
+    it { should_not allow_value('test').for(:email) }
+  end
 end
