31b20019dd12448ed0782a8a22283b3283a04721
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-05 20:25:11 -0700

    add the views and logic so a user can create a house after signing up or logging in

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,9 +1,9 @@
 class HousesController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :ensure_user_has_house
+  before_filter :ensure_user_has_house, only: [:show]
 
   def show
-    p current_user
+    @house = House.find(current_user.house.id)
   end
 
   def new
@@ -11,13 +11,21 @@ class HousesController < ApplicationController
   end
 
   def create
-    @house = House.create(params[:house])
-    current_user.house = @house
-    current_user.save
-    redirect_to house_path(@house)
+    @house = House.new(house_params)
+    if @house.save
+      current_user.house = @house
+      current_user.save
+      redirect_to house_path @house
+    else
+      render "houses/new"
+    end
   end
 
   private
+
+  def house_params
+    params.require(:house).permit(:name, :address)
+  end
 
   def ensure_user_has_house
     redirect_to new_house_path if current_user.house.nil?
