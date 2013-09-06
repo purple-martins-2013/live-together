25e4ca7f74f70e38a8a242da586ead38094146ba
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-05 21:30:00 -0700

    finish feature test for adding a new house

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,17 @@
 FactoryGirl.define do
 
   sequence :email do |n|
-    "email#{n}@factory.com"
+    "email#{n}#{rand(2000)}@factory.com"
   end
 
   factory :user do
     email
     password 'password'
     password_confirmation 'password'
+  end
+
+  factory :house do
+    name "House"
+    address "22 test st."
   end
 end
--- spec/features/user_can_create_house_spec.rb
+++ spec/features/user_can_create_house_spec.rb
@@ -3,5 +3,28 @@ require 'spec_helper'
 feature "user can create a house" do
 
   let(:user) { FactoryGirl.build(:user) }
+  let(:house) { FactoryGirl.build(:house) }
 
+  before do
+    sign_up_through_view(user)
+  end
+
+  it "should display the create house form" do
+    expect(page).to have_content "Create house"
+  end
+
+  it "should successfully create a new house with valid data" do
+    fill_in "house_name", with: house.name
+    fill_in "house_address", with: house.address
+    click_button "Create house"
+    expect(page).to have_content house.name
+    expect(page).to have_content house.address
+  end
+
+  it "should not create a house with invalid data" do
+    fill_in "house_name", with: house.name
+    click_button "Create house"
+    expect(page).to have_content 'error'
+    expect(page).to have_content 'Create house'
+  end
 end
