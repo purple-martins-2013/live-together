12f3a2e52a3bb2b716338e0bcc27cb242fa7eb88
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-05 19:31:32 -0700

    start adding views to create a new house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,7 +1,25 @@
 class HousesController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :ensure_user_has_house
 
   def show
-    
+    p current_user
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create(params[:house])
+    current_user.house = @house
+    current_user.save
+    redirect_to house_path(@house)
+  end
+
+  private
+
+  def ensure_user_has_house
+    redirect_to new_house_path if current_user.house.nil?
   end
 end
