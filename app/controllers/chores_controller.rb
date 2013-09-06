b0daa216f3703e1e8b948120fd2729b595e2af37
Colleen McCloskey <colleen.michelle.mccloskey@gmail.com>
2013-09-06 01:03:01 -0700

    association with user login in chore controller

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,5 +1,5 @@
 class ChoresController < ApplicationController
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
     @chores = Chore.all
--- spec/controllers/chore_controller_spec.rb
+++ spec/controllers/chore_controller_spec.rb
@@ -5,13 +5,18 @@ describe ChoresController do
   let(:chore) {create :chore}
 
   describe "#index" do
+    before(:each) do
+        @user = FactoryGirl.create(:user)
+    end
+
     it { should route(:get, '/chores').to(action: :index) }
 
     it "should list all chores" do
+
+      sign_in @user
       get :index
-      save_and_open_page
+
       assigns(:chores).should eq([chore])
-      # response.should render_template :index
     end
   end
 
@@ -19,39 +24,35 @@ describe ChoresController do
     it { should route(:get, 'chores/new').to(action: :new)}
   end
 
-  describe "#create" do 
+  describe "#create" do
+    before(:each) do
+        @user = FactoryGirl.create(:user)
+    end
     it { should route(:post, 'chores').to(action: :create)}
 
     context "when user logged in" do
-      before do
-        # user_session[:user_id] = user.id
-        sign_in user
-      end
 
 
       context "with chore and frequency filled out" do
-        xit "should create a chore" do
-          expect { post :create, {chore: attributes_for(:chore) } }.to change(Chore.all.last)
+        it "should create a chore" do
+          sign_in @user
+          expect { post :create, {chore: attributes_for(:chore) } }.to change{Chore.all.last}
         end
       end
 
       context "with chore and frequency not filled out" do
-        xit "should not create a chore" do
-          expect { post :create, { } }.not_to change(Chore.count)
+        it "should not create a chore" do
+          expect { post :create, { } }.not_to change{Chore.count}
         end
 
-        xit "should raise an error" do
-          post :create, {content: { } }
-          expect(response.status).to eq 422
-        end
       end
 
     end
 
     context "when user is not logged in" do
-      xit "should not create a chore" do
-        sign_out user
-        expect { post :create, {chore: attributes_for(:chore) } }.not_to change(Chore.count)
+      it "should not create a chore" do
+        # sign_out user
+        expect { post :create, {chore: attributes_for(:chore) } }.not_to change{Chore.count}
       end
     end
   end
