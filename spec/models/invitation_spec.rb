71c733202e64ca4006221a1cb31b870d63d3e9fd
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-08 00:05:00 -0700

    add model validations to spec

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 describe Invitation do
   describe "validations" do
     it { should validate_presence_of :email }
+    it { should validate_presence_of :user }
+    it { should validate_presence_of :house }
   end
 
   describe "relations" do
     it { should belong_to :house }
+    it { should belong_to :user }
   end
 end
