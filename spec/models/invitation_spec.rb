a18afdcaea4e45c3d9ca4311b14b36166f89ccc6
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-08 20:11:07 -0700

    update tests to respond to the new invitation model

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -4,11 +4,9 @@ describe Invitation do
   describe "validations" do
     it { should validate_presence_of :email }
     it { should validate_presence_of :user }
-    it { should validate_presence_of :house }
   end
 
   describe "relations" do
-    it { should belong_to :house }
     it { should belong_to :user }
   end
 end
