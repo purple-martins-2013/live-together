92f57703b7d97969378aa2ac3429755984a1ea93
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-10 15:44:17 -0700

    remove puts from controller

--- app/controllers/completed_chores_controller.rb
+++ app/controllers/completed_chores_controller.rb
@@ -1,7 +1,6 @@
 class CompletedChoresController < ApplicationController
   def index
     @completed_chores = current_house.completed_chores.order("completed_at DESC")
-    p @completed_chores
     respond_to do |format|
       format.html
       format.json { render json: @completed_chores }
