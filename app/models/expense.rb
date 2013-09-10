aa02d784a676b29a0737a73069c34e53d4a0ce94
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-10 10:50:25 -0700

    add validations for all the required attributes

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ActiveRecord::Base
-  validates_presence_of :name, :total_cents, :purchased_on
+  validates_presence_of :purchaser_id, :name, :total_cents, :purchased_on
 
   has_many :settlements
   has_many :contributors, through: :settlements, class_name: "User"
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Expense do
 
   describe "validations" do
+    it { should validate_presence_of :purchaser_id}
     it { should validate_presence_of :name }
     it { should validate_presence_of :total_cents }
     it { should validate_presence_of :purchased_on }
     it { should have_many :settlements }
     it { should have_many :contributors }
   end
-
 end
