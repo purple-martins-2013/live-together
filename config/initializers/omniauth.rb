6ba542639c65fbec79bf84c2f869c2e631066cbf
Colleen McCloskey <colleen.michelle.mccloskey@gmail.com>
2013-09-10 11:50:21 -0700

    Faraday::Error::ConnectionFailed error fixed on some machines

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE unless Rails.env.production?
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['APP_ID'], ENV['SECRET_KEY'], :scope => 'email', display: 'popup'
 end
