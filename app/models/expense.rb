260b02370c5526681dd38ea42d70230fdeeceae1
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-10 23:59:38 -0700

    update expense model to generate debts

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,30 +2,20 @@ class Expense < ActiveRecord::Base
   attr_accessor :contributor_ids
   validates_presence_of :purchaser_id, :name, :total_cents, :purchased_on
 
-  has_many :settlements
-  has_many :contributors, through: :settlements, class_name: "User"
+  has_many :debts
+  has_many :borrowers, through: :debts, class_name: "User"
   belongs_to :purchaser, class_name: "User"
 
-  after_save :create_settlements
+  after_save :create_debts
   # monetize :total_cents
 
-#using gem I wasn't sure if this is needed.
-  # def amount_in_cents
-  #   amount.fractional
-  # end
-
-# expense.users.count needs to be updated.
-  # def shared_amount_cents
-  #   total_cents / expense.users.count
-  # end
-
   private
-  def create_settlements
+  def create_debts
     contributors = User.where :id => self.contributor_ids
     # we account for the user that created the expense (self.purchaser) so we add 1 to the count of total contributors.
-    amount = self.total_cents/(contributors.count+1)
+    debt_amount = self.total_cents/(borrowers.count+1)
     contributors.each do |user|
-      self.settlements.create :contributor => user, :amount => amount
+      self.debts.create(borrower: user, lender: self.purchaser , amount_cents: debt_amount)
     end
   end
 end
