bfaed156d518e193f52f73da03170d09cf629610
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-10 21:51:50 -0700

    allow settlement creation with out a date_paid

--- app/models/settlement.rb
+++ app/models/settlement.rb
@@ -1,5 +1,5 @@
 class Settlement < ActiveRecord::Base
-  validates_presence_of :contributor, :expense, :amount_cents, :date_paid
+  validates_presence_of :contributor, :expense, :amount_cents
 
   belongs_to :contributor, class_name: "User"
   belongs_to :expense
