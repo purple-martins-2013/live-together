04d25cbe210ae0d9be9b1f614a44ec9ace6fafbc
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-11 18:49:31 -0700

    add validations for new columns

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ActiveRecord::Base
-  validates_presence_of :borrower, :lender, :description, :amount_cents
+  validates_presence_of :borrower, :lender, :description, :amount_cents, :method, :date
 
   belongs_to :borrower, class_name: "User"
   belongs_to :lender, class_name: "User"
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -8,6 +8,8 @@ describe Payment do
     it { should validate_presence_of :lender }
     it { should validate_presence_of :amount_cents }
     it { should validate_presence_of :description }
+    it { should validate_presence_of :method }
+    it { should validate_presence_of :date }
     it { should belong_to :borrower }
     it { should belong_to :lender }
 
