5c31b115b6e991caff390c0a9467af4fb26a711b
Fabian Uribe <fabian.uribe@gmail.com>
2013-09-11 18:48:14 -0700

    add columns to payments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130911065032) do
+ActiveRecord::Schema.define(version: 20130911182804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,8 @@ ActiveRecord::Schema.define(version: 20130911065032) do
     t.integer  "amount_cents", default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date"
+    t.string   "method"
   end
 
   create_table "users", force: true do |t|
