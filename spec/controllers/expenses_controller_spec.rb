707f558cc7e0f030b62bd33aba19b8b55e1f9928
Colleen McCloskey <colleen.michelle.mccloskey@gmail.com>
2013-09-12 15:31:02 -0700

    reword tests

--- spec/controllers/expenses_controller_spec.rb
+++ spec/controllers/expenses_controller_spec.rb
@@ -18,18 +18,18 @@ describe ExpensesController do
 
       it { should route(:get, '/expenses').to(action: :index) }
 
-      it "should list all your debts" do
+      it "lists all your debts" do
         get :index
         assigns(:user_debts).should eq(Debt.where( borrower: user))
       end
 
-      it "should list all house expenses" do
+      it "lists all house expenses" do
         get :index
         assigns(:house_expenses).should eq(Expense.where( purchaser_id: house.users))
 
       end
 
-      it "should render the 'index' view" do
+      it "renders the 'index' view" do
         get :index
         expect(response).to render_template 'expenses/index'
       end
@@ -76,7 +76,7 @@ describe ExpensesController do
         @expense = create(:expense)
       end
 
-      it "should delete an expense" do
+      it "deletes an expense" do
         expect {
           delete :destroy, id: @expense
         }.to change {Expense.count}.by(-1)
@@ -92,28 +92,28 @@ describe ExpensesController do
   context "when not logged in" do
 
     describe "#index" do
-      it "should redirect to the login form" do
+      it "redirects to the login form" do
         get :index
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
     describe "#show" do
-      it "should redirect to the login form" do
+      it "redirects to the login form" do
         get :show, id: expense
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
     describe "#new" do
-      it "should redirect to the login form" do
+      it "edirects to the login form" do
         get :new
         expect(response).to redirect_to(new_user_session_path)
       end
     end
 
     describe "#create" do
-      it "should not create a expense" do
+      it "does not create a expense" do
         expect { post :create, {expense: attributes_for(:expense) } }.not_to change{Chore.count}
       end
     end
