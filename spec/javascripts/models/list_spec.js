c4c9e44ead3ee4493d79d1cad69ad8c20bf4af26
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-13 01:29:53 -0700

    jasmine model tests

---
+++ spec/javascripts/models/list_spec.js
@@ -0,0 +1,13 @@
+describe("List model", function(){
+
+  var list = new LiveTogether.Models.List({id: 37});
+
+  it("has a collection of items", function(){
+    expect(list.items).not.toBe(undefined);
+  });
+
+  it("assigns the correct url to the collection of items", function(){
+    expect(list.items.url).toBe("/grocery_lists/37/grocery_items");
+  });
+
+});
